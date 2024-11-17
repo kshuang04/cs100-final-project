2317467c2eb62d9768490ea4748b45a15480ab23
JosiahKou <josiahkou28@gmail.com>
2024-11-16 20:11:04 -0800

    stage and region added

--- header/enemy.hpp
+++ header/enemy.hpp
@@ -5,34 +5,43 @@
 
 using namespace std;
 
-class Enemy {
-    private:
-        int health;
-        int maxHealth;
-        int attackPower;
-        int defense;
-        string description;
-        int expAmount;
-        int goldAmount;
-        bool alive;
-        string name;
-    public:
-        //DONT USE THIS CONSTRUCTOR
-        //this is only for enemy insertion in level class
-        Enemy(int maxHealth, int defense, int attackPower, int exp, int gold, string enemyName);
-        
-        Enemy();
+class Enemy
+{
+private:
+    int health;
+    int maxHealth;
+    int attackPower;
+    int defense;
+    string description;
+    int expAmount;
+    int goldAmount;
+    bool alive;
+    string name;
 
-        // ~Enemy(); //?
-        int attack();
-        void gotAttack(int playerDamage);
-        void heal(int healthIncrease); //?
-        void releaseDrops();
-        bool isAlive();
-        void setDescription(string);
-        string getName();
-        int getHealth();
-        
+    // 1 = early, 2 = mid, 3 = late
+    int stage;
+    // 1 = plains, 2 = forest, 3 = castle, 4 = grave, 5 = lake
+    int region;
+
+public:
+    // DONT USE THIS CONSTRUCTOR
+    // this is only for enemy insertion in level class
+    Enemy(int maxHealth, int defense, int attackPower, int exp, int gold, string enemyName);
+
+    Enemy();
+
+    // ~Enemy(); //?
+    int attack();
+    void gotAttack(int playerDamage);
+    void heal(int healthIncrease); //?
+    void releaseDrops();
+    bool isAlive();
+    void setDescription(string);
+    string getName();
+    int getHealth();
+
+    int getStage();
+    int getRegion();
 };
 
 #endif
