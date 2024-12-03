65384e77252e8c47974acd3183ace905b734880b
O1iver-R <phone8848ti@gmail.com>
2024-12-02 20:47:54 -0800

    add comments to event class

--- src/Event.cpp
+++ src/Event.cpp
@@ -1,23 +1,26 @@
 #include "../header/Event.hpp"
 #include <iostream>
 
+//constructor
 MiniGame::MiniGame(const std::string& gameName) : name(gameName), rewardChest(nullptr) {}
 
+//destructor
 MiniGame::~MiniGame() {
     delete rewardChest; // only need to delete the chest
 }
 
+//get the name of the game
 std::string MiniGame::getName() const {
     return name;
 }
 
-
+// generate the reward : item + gold
+// basically what it does is creating a chest pointed by rewardChest
 void MiniGame::generateReward() {
-    // generate random amount of gold
     GenerateItems ItemGenerated;
-
-
+    // generate random amount of gold
     int goldAmount = rand() % 100 + 1;
+    // generate a vector of items directed copy from item
     std::vector<Item*> items = ItemGenerated.generateItem();
 
     rewardChest = new Chest(10, items, goldAmount);
